66acdbcb2afba648bb428b97ac4a93dbe9f20860
LightGive <roi.akase@outlook.jp>
2018-11-28 07:40:58 +0900

    数字の画像追加、シーン修正

---
+++ SpriteNumber/Assets/LightGive/Utilities/SpriteNumber/Scripts/Editor/SpriteNumberEditor.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(SpriteNumber))]
+public class SpriteNumberEditor : Editor
+{
+	public override void OnInspectorGUI()
+	{
+
+	}
+}
--- SpriteNumber/Assets/LightGive/Utilities/SpriteNumber/Scripts/SpriteNumber.cs
+++ SpriteNumber/Assets/LightGive/Utilities/SpriteNumber/Scripts/SpriteNumber.cs
@@ -4,13 +4,76 @@ using UnityEngine;
 
 public class SpriteNumber : MonoBehaviour
 {
-	void Start()
-	{
+	private const int MinDigit = 1;
+	private const int MaxDigit = 10;
 
+	[SerializeField]
+	private Sprite[] m_spriteNumbers = new Sprite[10];
+	[SerializeField]
+	private Sprite m_spriteDot;
+	[SerializeField, Range(MinDigit, MaxDigit)]
+	private int m_maxDigit = 3;
+	[SerializeField]
+	private float m_offset = 1.0f;
+	[SerializeField]
+	private bool m_isDisplayZero;
+
+	private SpriteRenderer[] m_spriteRenderers = new SpriteRenderer[MaxDigit];
+
+	private void Start()
+	{
+		m_spriteRenderers = new SpriteRenderer[m_maxDigit];
+		for (int i = 0; i < m_maxDigit; i++)
+		{
+			var obj = new GameObject("Number" + i.ToString("0"));
+			obj.transform.SetParent(transform);
+			m_spriteRenderers[i] = obj.AddComponent<SpriteRenderer>();
+		}
 	}
 
-	void Update()
+	private void Update()
 	{
+		if (Input.GetKeyDown(KeyCode.Space))
+		{
+			var ran = Random.Range(0, 99);
+			Debug.Log(ran);
+			SetNumber(ran);
+		}
+	}
 
+	public void SetNumber(int _val)
+	{
+		var digit = _val;
+
+		//要素数0には１桁目の値が格納
+		List<int> number = new List<int>();
+		while (digit != 0)
+		{
+			_val = digit % 10;
+			digit = digit / 10;
+			number.Add(_val);
+		}
+
+		//表示
+		for (int i = 0; i < m_spriteRenderers.Length; i++)
+		{
+			if (number.Count <= i && i != number.Count - 1)
+			{
+				if (m_isDisplayZero)
+				{
+					m_spriteRenderers[i].gameObject.SetActive(true);
+					m_spriteRenderers[i].sprite = m_spriteNumbers[0];
+				}
+				else
+				{
+					m_spriteRenderers[i].gameObject.SetActive(false);
+				}
+			}
+			else
+			{
+				m_spriteRenderers[i].gameObject.SetActive(true);
+				m_spriteRenderers[i].sprite = m_spriteNumbers[number[i]];
+			}
+		}
 	}
 }
